cf592c343b1839129c87480a44895c69187f23d8
aftesh <fantan377@gmail.com>
2025-07-23 23:54:05 +0300

    walk anim

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 var speed = 100
 var speedr = speed * 2
 
+
+@onready var anim = $"player anim"
 func walk():
 	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	if Input.is_key_pressed(KEY_SHIFT):
@@ -10,6 +12,9 @@ func walk():
 	else:
 		velocity = input_direction * speed
 	move_and_slide()
-
+	if input_direction == Vector2(0, 0):
+		anim.play("idle")
+	else:
+		anim.play("walk")
 func _physics_process(delta):
 	walk()
--- scripts/test_lvl.gd
+++ scripts/test_lvl.gd
@@ -2,4 +2,4 @@ extends Node2D
 
 func _process(delta):
 	if Input.is_key_pressed(KEY_Q):
-		get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+		get_tree().quit()
