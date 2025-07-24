d237d1c136c13297f275ae02eb81eeb9e7be4709
aftesh <fantan377@gmail.com>
2025-07-24 20:19:04 +0300

    furnes update

--- scripts/furnes.gd
+++ scripts/furnes.gd
@@ -1,10 +1,23 @@
 extends StaticBody2D
 
+
+
 var is_smelting = false 
 @onready var anim = $anim
+@onready var area = $Area2D
 
-func smelting():
+func smelting_anim():
 	if is_smelting:
 		anim.play("work")
 	else:
 		anim.play("idle")
+
+func _process(delta: float) -> void:
+#	smelting()
+	smelting_anim()
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	is_smelting = true
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	is_smelting = false
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -3,8 +3,12 @@ extends Node2D
 
 
 func _on_play_test_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/map.tscn")
+	get_tree().change_scene_to_file("res://scenes/test_lvl.tscn")
 
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/map.tscn")
